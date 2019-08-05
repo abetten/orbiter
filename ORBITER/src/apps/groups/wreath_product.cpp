ddb8e1b91d49d7988ade1e9e0499de62074f5273
RoySRC <roysajeeb4@gmail.com>
2019-08-04 19:37:36 -0600

    broke up matrix multiplication for wreath product

--- ORBITER/src/apps/groups/wreath_product.cpp
+++ ORBITER/src/apps/groups/wreath_product.cpp
@@ -982,138 +982,143 @@ void tensor_product::init(int argc, const char **argv,
 		exit(1);
 	}
 
-	schreier *Sch;
-
-	Sch = NEW_OBJECT(schreier);
-
-	cout << "before Sch->init_images_only" << endl;
-	Sch->init_images_only(nb_gens,
-			degree, result, verbose_level);
-	cout << "computing point orbits from image table:" << endl;
-	Sch->compute_all_point_orbits(verbose_level);
-
-	cout << "time check: ";
-	time_check(cout, t0);
-	cout << endl;
-
-
-	cout << "computing point orbits from image table done" << endl;
-	cout << "We found " << Sch->nb_orbits << " orbits" << endl;
-
-
-#if 0
-	A->perform_tests(SG, verbose_level);
-#endif
-
-	exit(0);
-
-
-	Gen = NEW_OBJECT(poset_classification);
-
-	Gen->read_arguments(argc, argv, 0);
-
-	//Gen->prefix[0] = 0;
-	sprintf(Gen->fname_base, "wreath_%d_%d_%d", nb_factors, n, q);
-
-
-	Gen->depth = depth;
-
-	VS = NEW_OBJECT(vector_space);
-	VS->init(F,
-			vector_space_dimension /* dimension */,
-			verbose_level - 1);
-	VS->init_rank_functions(
-			wreath_rank_point_func,
-			wreath_unrank_point_func,
-			this,
-			verbose_level - 1);
-
-
-	Poset = NEW_OBJECT(poset);
-	Poset->init_subspace_lattice(
-			A0, A,
-			SG,
-			VS,
-			verbose_level);
-
-	if (f_v) {
-		cout << "tensor_product::init before Gen->init" << endl;
-		}
-	Gen->init(Poset, Gen->depth /* sz */, verbose_level);
-	if (f_v) {
-		cout << "tensor_product::init after Gen->init" << endl;
-		}
-
-
-	Gen->f_print_function = TRUE;
-	Gen->print_function = wreath_product_print_set;
-	Gen->print_function_data = this;
-
-	int nb_nodes = 1000;
-
-	if (f_v) {
-		cout << "tensor_product::init "
-				"before Gen->init_poset_orbit_node" << endl;
-		}
-	Gen->init_poset_orbit_node(nb_nodes, verbose_level - 1);
-	if (f_v) {
-		cout << "tensor_product::init "
-				"calling Gen->init_root_node" << endl;
-		}
-	Gen->root[0].init_root_node(Gen, verbose_level - 1);
-
-	//int schreier_depth;
-	int f_use_invariant_subset_if_available;
-	int f_debug;
-
-	//schreier_depth = Gen->depth;
-	f_use_invariant_subset_if_available = TRUE;
-	f_debug = FALSE;
-
-	//int t0 = os_ticks();
-
-	if (f_v) {
-		cout << "tensor_product::init before Gen->main" << endl;
-		cout << "A=";
-		A->print_info();
-		cout << "A0=";
-		A0->print_info();
-		}
-
-
-	//Gen->f_allowed_to_show_group_elements = TRUE;
-
-	Gen->main(t0,
-		Gen->depth,
-		f_use_invariant_subset_if_available,
-		f_debug,
-		verbose_level);
-
-	set_of_sets *SoS;
-
-	SoS = Gen->Schreier_vector_handler->get_orbits_as_set_of_sets(
-			Gen->root[0].Schreier_vector, verbose_level);
-
-	SoS->sort_all(verbose_level);
-	cout << "orbits at level 1:" << endl;
-	SoS->print_table();
-
-	for (i = 0; i < SoS->nb_sets; i++) {
-		cout << "Orbit " << i << " has size " << SoS->Set_size[i] << " : ";
-		int_vec_print(cout, SoS->Sets[i], SoS->Set_size[i]);
-		cout << endl;
-		for (j = 0; j < SoS->Set_size[i]; j++) {
-			a = SoS->Sets[i][j];
-			cout << j << " : " << a << " : ";
-			F->PG_element_unrank_modified(v, 1, vector_space_dimension, a);
-			int_vec_print(cout, v, vector_space_dimension);
-			cout << endl;
-		}
-	}
-
-	if (f_v) {
-		cout << "tensor_product::init after Gen->main" << endl;
-	}
+//	schreier *Sch;
+//
+//	Sch = NEW_OBJECT(schreier);
+//
+//	cout << "before Sch->init_images_only" << endl;
+//	Sch->init_images_only(nb_gens,
+//			degree, result, verbose_level);
+//
+//	cout << "nb_gens: " << nb_gens << endl;
+//
+//	cout << "computing point orbits from image table:" << endl;
+//	Sch->compute_all_point_orbits(0);
+//
+//	Sch->print_orbit_lengths(cout);
+//
+//	cout << "time check: ";
+//	time_check(cout, t0);
+//	cout << endl;
+//
+//
+//	cout << "computing point orbits from image table done" << endl;
+//	cout << "We found " << Sch->nb_orbits << " orbits" << endl;
+//
+//
+//#if 0
+//	A->perform_tests(SG, verbose_level);
+//#endif
+//
+//	exit(0);
+//
+//
+//	Gen = NEW_OBJECT(poset_classification);
+//
+//	Gen->read_arguments(argc, argv, 0);
+//
+//	//Gen->prefix[0] = 0;
+//	sprintf(Gen->fname_base, "wreath_%d_%d_%d", nb_factors, n, q);
+//
+//
+//	Gen->depth = depth;
+//
+//	VS = NEW_OBJECT(vector_space);
+//	VS->init(F,
+//			vector_space_dimension /* dimension */,
+//			verbose_level - 1);
+//	VS->init_rank_functions(
+//			wreath_rank_point_func,
+//			wreath_unrank_point_func,
+//			this,
+//			verbose_level - 1);
+//
+//
+//	Poset = NEW_OBJECT(poset);
+//	Poset->init_subspace_lattice(
+//			A0, A,
+//			SG,
+//			VS,
+//			verbose_level);
+//
+//	if (f_v) {
+//		cout << "tensor_product::init before Gen->init" << endl;
+//		}
+//	Gen->init(Poset, Gen->depth /* sz */, verbose_level);
+//	if (f_v) {
+//		cout << "tensor_product::init after Gen->init" << endl;
+//		}
+//
+//
+//	Gen->f_print_function = TRUE;
+//	Gen->print_function = wreath_product_print_set;
+//	Gen->print_function_data = this;
+//
+//	int nb_nodes = 1000;
+//
+//	if (f_v) {
+//		cout << "tensor_product::init "
+//				"before Gen->init_poset_orbit_node" << endl;
+//		}
+//	Gen->init_poset_orbit_node(nb_nodes, verbose_level - 1);
+//	if (f_v) {
+//		cout << "tensor_product::init "
+//				"calling Gen->init_root_node" << endl;
+//		}
+//	Gen->root[0].init_root_node(Gen, verbose_level - 1);
+//
+//	//int schreier_depth;
+//	int f_use_invariant_subset_if_available;
+//	int f_debug;
+//
+//	//schreier_depth = Gen->depth;
+//	f_use_invariant_subset_if_available = TRUE;
+//	f_debug = FALSE;
+//
+//	//int t0 = os_ticks();
+//
+//	if (f_v) {
+//		cout << "tensor_product::init before Gen->main" << endl;
+//		cout << "A=";
+//		A->print_info();
+//		cout << "A0=";
+//		A0->print_info();
+//		}
+//
+//
+//	//Gen->f_allowed_to_show_group_elements = TRUE;
+//
+//	Gen->main(t0,
+//		Gen->depth,
+//		f_use_invariant_subset_if_available,
+//		f_debug,
+//		verbose_level);
+//
+//	set_of_sets *SoS;
+//
+//	SoS = Gen->Schreier_vector_handler->get_orbits_as_set_of_sets(
+//			Gen->root[0].Schreier_vector, verbose_level);
+//
+//	SoS->sort_all(verbose_level);
+//	cout << "orbits at level 1:" << endl;
+//	SoS->print_table();
+//
+//	for (i = 0; i < SoS->nb_sets; i++) {
+//		cout << "Orbit " << i << " has size " << SoS->Set_size[i] << " : ";
+//		int_vec_print(cout, SoS->Sets[i], SoS->Set_size[i]);
+//		cout << endl;
+//		for (j = 0; j < SoS->Set_size[i]; j++) {
+//			a = SoS->Sets[i][j];
+//			cout << j << " : " << a << " : ";
+//			F->PG_element_unrank_modified(v, 1, vector_space_dimension, a);
+//			int_vec_print(cout, v, vector_space_dimension);
+//			cout << endl;
+//		}
+//	}
+//
+//	if (f_v) {
+//		cout << "tensor_product::init after Gen->main" << endl;
+//	}
 }
 
 
@@ -1157,6 +1162,14 @@ void wreath_product_print_set(ostream &ost,
 	}
 }
 
+
+//template <typename T = int>
+int root (int* S, int i) {
+	while (S[i] != -1) i = S[i];
+	return i;
+}
+
+
 void wreath_product_orbits_CUDA(wreath_product* W,
 								strong_generators* SG,
 								action* A,
@@ -1193,31 +1206,36 @@ void wreath_product_orbits_CUDA(wreath_product* W,
 	cout << "mtx_n=" << mtx_n << endl;
 	cout << "SG->gens->len * mtx_n=" << SG->gens->len * mtx_n << endl;
 
+	linalg::Matrix<int> v (mtx_n, 1);
 
-	// matrix M contains in the rows the coordinates of all points
-	// of the projective geometry:
-	// M has size W->degree_of_tensor_action x mtx_n
-
-	linalg::Matrix<int> M (W->degree_of_tensor_action, mtx_n);
-	PGL_Vector_unrank_Matrix (M, mtx_n, W->q, W->degree_of_tensor_action);
 
 	// matrix N contains the matrices of all projectivities
 	// which generate the group, stacked on top of each other.
 	// So, N has size (SG->gens->len * mtx_n) x mtx_n
 
-	linalg::Matrix<int> N (SG->gens->len * mtx_n, mtx_n);
-	for (size_t i = 0; i < N.nrows; ++i) {
-		for (size_t j = 0; j < N.ncols; ++j) {
-			N(i, j) = generator_stack [i * N.ncols + j];
-		}
+
+	vector<linalg::Matrix<int>> N (SG->gens->len);
+	for (size_t h = 0; h < N.size(); ++h) {
+		N[h].INIT(mtx_n, mtx_n);
+
+		for (size_t i=0; i < mtx_n; ++i)
+			for (size_t j = 0; j < mtx_n; ++j) {
+				N[h].matrix_[i*mtx_n+j] = generator_stack [h * mtx_n2 + i * mtx_n + j];
+			}
+
 	}
 
-//	linalg::Matrix<int> N ;
-//	N.set_data_reference (generator_stack, SG->gens->len * mtx_n, mtx_n);
-//	N.dealloc_data_on_destruct = false;
+	// Print the matrices N
+	for (size_t h=0; h<N.size(); ++h) {
+		printf("=========================================================\n");
+		printf("h = %ld\n", h);
+		printf("=========================================================\n");
 
-	// MN = M * N:
-	linalg::Matrix<int> MN (M.nrows, N.ncols);
+		linalg::print(N[h]);
+
+		printf("=========================================================\n");
+	}
+
 
 
 	// result is the ranks of the images.
@@ -1228,35 +1246,108 @@ void wreath_product_orbits_CUDA(wreath_product* W,
 
 	// perform the parallel matrix multiplication on the GPU:
 
-	cuda_dot(M, N, MN, perms, result, W->q);
 
-//	cout << "result:" << endl;
-//	int_matrix_print(result, SG->gens->len, W->degree_of_tensor_action);
+//	int* v = NEW_int (MN.ncols);
 
 
-	combinatorics_domain Combi;
+	int* S = NEW_int (W->degree_of_tensor_action);
+	int* T = NEW_int (W->degree_of_tensor_action);
 
-	for (size_t i = 0; i < SG->gens->len; i++) {
-		cout << "testing result " << i << " / " << SG->gens->len << ": ";
-		if (Combi.is_permutation(
-				result + i * W->degree_of_tensor_action,
-				W->degree_of_tensor_action)) {
-			cout << "OK" << endl;
+	int block_size = 1L << 28; // pow(2, 28) ints = 1024 MB
+	int nb_blocks = (W->degree_of_tensor_action + block_size - 1) / block_size;
+
+	cout << "nb_blocks=" << nb_blocks << endl;
+//	memset(S, -1, sizeof(S)*W->degree_of_tensor_action);
+	for (size_t i=0; i<W->degree_of_tensor_action; ++i) S[i] = -1;
+
+
+
+	for (size_t h=0; h < N.size(); ++h) {
+		cout << "hh=" << h << endl;
+
+		for (size_t b=0; b<nb_blocks; ++b) {
+			cout << "b=" << b << endl;
+
+			const size_t l = std::min((b + 1) * block_size,
+										(unsigned long)W->degree_of_tensor_action) - b*block_size;
+			cout << "l=" << l << endl;
+
+			linalg::Matrix<int> M  (l, mtx_n);
+			linalg::Matrix<int> MN (l, mtx_n);
+
+			for (size_t i=0; i<l; ++i) {
+				W->F->PG_element_unrank_modified (v.matrix_, 1, mtx_n, b * block_size + i) ;
+				for (size_t j=0; j<mtx_n; ++j)
+					M(i,j) = v(j, 0);
+			}
+
+
+			// Matrix Multiply
+			MN.reset_entries();
+//			linalg::cpu_mod_mat_mul_AB (M, N[h], MN, W->q);
+
+			linalg::cuda_mod_mat_mul (M, N[h], MN, W->q);
+			M.UninitializeOnGPU();
+			N[h].UninitializeOnGPU();
+			MN.UninitializeOnGPU();
+
+
+			for (size_t i=0; i<l; ++i) {
+				for (size_t j=0; j<mtx_n; ++j) {
+					int a = perms[h * mtx_n + j];
+					v.matrix_[a*v.alloc_cols] = MN (i, j);
+
+				}
+				int res;
+				W->F->PG_element_rank_modified (v.matrix_, 1, mtx_n, res) ;
+				T [b * block_size + i] = res;
+			}
+
 		}
-		else {
-			cout << "not OK" << endl;
+
+		for (size_t i=0; i < W->degree_of_tensor_action; ++i) {
+			int t = T[i];
+			int r1 = root(S, i);
+			int r2 = root(S, t);
+
+			if (r1 != r2) {
+				if (r1 < r2) S[r2] = r1; else S[r1] = r2;
+			}
 		}
+
 	}
-	cout << "We found " << SG->gens->len << " permutations of "
-			"degree " << W->degree_of_tensor_action << endl;
+
+	int nb_orbits = 0;
+	for (size_t i=0; i < W->degree_of_tensor_action; ++i) {
+		if (S[i] == -1) ++nb_orbits;
+	}
+	printf("nb_orbits: %d\n", nb_orbits);
+	return;
 
 
-	cout << __FILE__ << ":" << __LINE__ << endl;
-	//exit(0);
-
-	FREE_int(generator_stack);
-	FREE_int(perms);
-	cout << "wreath_product_orbits_CUDA done" << endl;
+//	combinatorics_domain Combi;
+//
+//	for (size_t i = 0; i < SG->gens->len; i++) {
+//		cout << "testing result " << i << " / " << SG->gens->len << ": ";
+//		if (Combi.is_permutation(
+//				result + i * W->degree_of_tensor_action,
+//				W->degree_of_tensor_action)) {
+//			cout << "OK" << endl;
+//		}
+//		else {
+//			cout << "not OK" << endl;
+//		}
+//	}
+//	cout << "We found " << SG->gens->len << " permutations of "
+//			"degree " << W->degree_of_tensor_action << endl;
+//
+//
+//	cout << __FILE__ << ":" << __LINE__ << endl;
+//	//exit(0);
+//
+//	FREE_int(generator_stack);
+//	FREE_int(perms);
+//	cout << "wreath_product_orbits_CUDA done" << endl;
 #else
 	nb_gens = 0;
 	degree = 0;
