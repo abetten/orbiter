fb16a3b7a038a12240919a2e434f279b84c3610f
Anton Betten  <betten@math.colostate.edu>
2020-05-30 15:52:28 -0600

    added classes related to diophant

--- src/lib/foundations/solvers/diophant_create.cpp
+++ src/lib/foundations/solvers/diophant_create.cpp
@@ -29,15 +29,37 @@ diophant_create::~diophant_create()
 }
 
 void diophant_create::init(
-		diophant_description *description,
+		diophant_description *Descr,
 		int verbose_level)
 {
 	int f_v = (verbose_level >= 1);
 
 	if (f_v) {
-		cout << "linear_group::init" << endl;
+		cout << "diophant_create::init" << endl;
 	}
-	diophant_create::Descr = description;
+	diophant_create::Descr = Descr;
+
+
+	finite_field *F = NULL;
+
+	if (Descr->f_q) {
+		F = NEW_OBJECT(finite_field);
+
+		if (Descr->f_override_polynomial) {
+			cout << "creating finite field of order q=" << Descr->input_q
+					<< " using override polynomial " << Descr->override_polynomial << endl;
+			F->init_override_polynomial(Descr->input_q,
+					Descr->override_polynomial, verbose_level);
+		}
+		else {
+			cout << "diophant_create::init creating finite field "
+					"of order q=" << Descr->input_q
+					<< " using the default polynomial (if necessary)" << endl;
+			F->init(Descr->input_q, 0);
+		}
+
+	}
+
 
 	if (Descr->f_maximal_arc) {
 
@@ -45,7 +67,7 @@ void diophant_create::init(
 
 		P = NEW_OBJECT(projective_space);
 
-		P->init(2, Descr->F,
+		P->init(2, F,
 				TRUE /* f_init_incidence_structure */,
 				verbose_level);
 
@@ -66,13 +88,131 @@ void diophant_create::init(
 
 		FREE_OBJECT(P);
 	}
+	else if (Descr->f_from_scratch) {
+
+		int m, n, i, j;
+
+		m = Descr->from_scratch_m;
+		n = Descr->from_scratch_n;
+		D = NEW_OBJECT(diophant);
+
+		D->open(m, n);
+
+		if (Descr->f_from_scratch_coefficient_matrix) {
+			int *A;
+			int sz;
+
+			int_vec_scan(Descr->from_scratch_A_text, A, sz);
+			if (sz != m * n) {
+				cout << "sz != m * n" << endl;
+				exit(1);
+			}
+			for (i = 0; i < m; i++) {
+				for (j = 0; j < n; j++) {
+					D->Aij(i, j) = A[i * n + j];
+				}
+			}
+			FREE_int(A);
+		}
+		else {
+			cout << "warning, coefficient matrix is not specified" << endl;
+		}
+
+		if (Descr->f_from_scratch_RHS) {
+			int *RHS;
+			int sz;
+
+			int_vec_scan(Descr->from_scratch_RHS_text, RHS, sz);
+			if (sz != m) {
+				cout << "sz != m" << endl;
+				exit(1);
+			}
+			for (i = 0; i < m; i++) {
+				D->RHS[i] = RHS[i];
+				//D->type[i] = t_EQ;
+			}
+			FREE_int(RHS);
+
+		}
+		else {
+			cout << "warning, RHS is not specified" << endl;
+		}
+
+		if (Descr->f_from_scratch_RHS_type) {
+			int *RHS_type;
+			int sz;
+
+			int_vec_scan(Descr->from_scratch_RHS_type_text, RHS_type, sz);
+			if (sz != m) {
+				cout << "sz != m" << endl;
+				exit(1);
+			}
+			for (i = 0; i < m; i++) {
+				if (RHS_type[i] == 1) {
+					D->type[i] = t_EQ;
+				}
+				else if (RHS_type[i] == 2) {
+					D->type[i] = t_LE;
+				}
+				else if (RHS_type[i] == 3) {
+					D->type[i] = t_ZOR;
+				}
+				else {
+					cout << "unknown type" << endl;
+					exit(1);
+				}
+			}
+			FREE_int(RHS_type);
+		}
+		else {
+			cout << "warning, RHS_type is not specified, assuming equalities" << endl;
+			for (i = 0; i < m; i++) {
+				D->type[i] = t_EQ;
+			}
+		}
+
+		if (Descr->f_from_scratch_RHS_type) {
+			int *x_max;
+			int sz;
+
+			int_vec_scan(Descr->from_scratch_x_max_text, x_max, sz);
+			if (sz != n) {
+				cout << "sz != n" << endl;
+				exit(1);
+			}
+			for (j = 0; j < n; j++) {
+				D->x_max[j] = x_max[j];
+			}
+			FREE_int(x_max);
+		}
+		else {
+			cout << "warning, x_max is not specified" << endl;
+		}
+
+		if (Descr->f_from_scratch_sum) {
+
+			D->f_has_sum = TRUE;
+			D->sum = Descr->from_scratch_sum;
+		}
+
+
+
+		char fname[1000];
+		file_io Fio;
+
+		sprintf(fname, "%s.diophant", Descr->from_scratch_label);
+		D->save_in_general_format(fname, verbose_level);
+		cout << "Written file " << fname << " of size " << Fio.file_size(fname) << endl;
+
+	}
 	else {
 		cout << "diophant_create::init type not specified" << endl;
 		exit(1);
 	}
 
-	if (f_v) {
-		cout << "diophant_create::init" << endl;
+
+	if (F) {
+		FREE_OBJECT(F);
 	}
 
 	if (f_v) {
--- src/lib/foundations/solvers/diophant_description.cpp
+++ src/lib/foundations/solvers/diophant_description.cpp
@@ -23,14 +23,35 @@ diophant_description::diophant_description()
 	input_q = 0;
 	f_override_polynomial = FALSE;
 	override_polynomial = NULL;
-	F = NULL;
+	//F = NULL;
 
 	f_maximal_arc = FALSE;
 	maximal_arc_sz = 0;
 	maximal_arc_d = 0;
 	maximal_arc_secants_text = NULL;
 	external_lines_as_subset_of_secants_text = NULL;
+
+	f_from_scratch = FALSE;
+	from_scratch_label = NULL;
+	from_scratch_m = 0;
+	from_scratch_n = 0;
+
+	f_from_scratch_coefficient_matrix = FALSE;
+	from_scratch_A_text = NULL;
+
+	f_from_scratch_RHS = FALSE;
+	from_scratch_RHS_text = NULL;
+
+	f_from_scratch_RHS_type = FALSE;
+	from_scratch_RHS_type_text = NULL;
+
+	f_from_scratch_x_max = FALSE;
+	from_scratch_x_max_text = NULL;
+
+	f_from_scratch_sum = FALSE;
+	from_scratch_sum = 0;
 }
+
 
 diophant_description::~diophant_description()
 {
@@ -55,6 +76,40 @@ int diophant_description::read_arguments(
 			cout << "-maximal_arc " << maximal_arc_sz << " " << maximal_arc_d
 					<< " " << maximal_arc_secants_text
 					<< " " << external_lines_as_subset_of_secants_text << endl;
+		}
+		else if (strcmp(argv[i], "-from_scratch") == 0) {
+			f_from_scratch = TRUE;
+			from_scratch_label = argv[++i];
+			from_scratch_m = atoi(argv[++i]);
+			from_scratch_n = atoi(argv[++i]);
+			cout << "-from_scratch " << from_scratch_label
+					<< " " << from_scratch_m << " " << from_scratch_n
+					<< endl;
+		}
+		else if (strcmp(argv[i], "-coefficient_matrix") == 0) {
+			f_from_scratch_coefficient_matrix = TRUE;
+			from_scratch_A_text = argv[++i];
+			cout << "-from_scratch_coefficient_matrix " << from_scratch_A_text << endl;
+		}
+		else if (strcmp(argv[i], "-RHS") == 0) {
+			f_from_scratch_RHS = TRUE;
+			from_scratch_RHS_text = argv[++i];
+			cout << "-from_scratch_RHS " << from_scratch_RHS_text << endl;
+		}
+		else if (strcmp(argv[i], "-RHS_type") == 0) {
+			f_from_scratch_RHS_type = TRUE;
+			from_scratch_RHS_type_text = argv[++i];
+			cout << "-from_scratch_RHS_type " << from_scratch_RHS_type_text << endl;
+		}
+		else if (strcmp(argv[i], "-x_max") == 0) {
+			f_from_scratch_x_max = TRUE;
+			from_scratch_x_max_text = argv[++i];
+			cout << "-from_scratch_x_max " << from_scratch_x_max_text << endl;
+		}
+		else if (strcmp(argv[i], "-has_sum") == 0) {
+			f_from_scratch_sum = TRUE;
+			from_scratch_sum = atoi(argv[++i]);
+			cout << "-from_scratch_sum " << from_scratch_sum << endl;
 		}
 		else if (strcmp(argv[i], "-q") == 0) {
 			f_q = TRUE;
